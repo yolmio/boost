b1643f20a105e8e45e4e855ee8ed1fcee07a8c24
nuvacore <nuvacore@nuvanti.com>
2023-07-15 13:56:58 -0400

    updated hello world

--- examples/hello_world/app.ts
+++ examples/hello_world/app.ts
@@ -3,7 +3,7 @@ import { addEnum, addPage, addTable } from "@yolm/boost/modelHelpers";
 import { model } from "@yolm/boost/singleton";
 
 model.name = "hello_world";
-model.title = "Hello World";
+model.title = "Hello World App";
 model.dbRunMode = "BrowserSync";
 
 // db
